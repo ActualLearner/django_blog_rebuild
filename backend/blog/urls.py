6bef08d1256f8f143b1cb0648a15f43a4d56ffec
ActualLearner <abdellahshafi7@gmail.com>
2025-08-02 18:22:58 +0300

    feat; Implement custom permission

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -3,9 +3,9 @@ from .views import *
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'post', PostViewSet, basename='post')
-router.register(r'comment', CommentViewSet, basename='comment')
-router.register(r'user', UserViewSet, basename='user')
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'comments', CommentViewSet, basename='comment')
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = []
 urlpatterns += router.urls
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
-from .models import *
-from .serializers import *
+from .models import Post, Comment, User
+from .serializers import PostSerializer, CommentSerializer, UserSerializer
+from .permissions import isOwnerOrReadOnly
 
 # Create your views here.
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, isOwnerOrReadOnly]
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
